3d320ed473dc7fc95fa7f965f8d86c43ecc52829
Sami Souayah <souays@bu.edu>
2024-12-24 12:13:32 -0500

    Stuff

--- PyTorch/Dsets/training_dataset.py
+++ PyTorch/Dsets/training_dataset.py
@@ -11,12 +11,13 @@ class Training_Dataset():
         self.current = dt.now().date()
         self.data = data = yf.download("AAPL", period='6mo')
         self.seql = 50
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
         self.X_train, self.X_test, self.Y_test, self.Y_train = None
 
     def create_data(self):
         closing_prices = self.data['Close']
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        normalized_prices = scaler.fit_transform(closing_prices.values.reshape(-1, 1))
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        normalized_prices = self.scaler.fit_transform(closing_prices.values.reshape(-1, 1))
         normalized_prices = pd.DataFrame(normalized_prices, columns=["Close"])
         X_train, Y_train = [], []
         for i in range(len(normalized_prices) - self.seql):
--- PyTorch/models/eval.py
+++ PyTorch/models/eval.py
@@ -1,19 +1,21 @@
 import torch
 from nn import LSTMModel  
-from Dsets.training_dataset import scaler
-from Dsets.dataset import X_input
+from Dsets.training_dataset import Training_Dataset
+from Dsets.dataset import Dataset
 
 
-X_input = torch.tensor(X_input, dtype=torch.float32)
-
-model = LSTMModel(input_size=1, hidden_size=50, num_layers=2)
-
-model.load_state_dict(torch.load("best_lstm_model.pth", weights_only=True))
-model.eval() 
-
-with torch.no_grad():
-    predicted_price = model(X_input)
-
-predicted_price = scaler.inverse_transform(predicted_price.detach().numpy())
-
-print(f"Predicted stock price for the next day: {predicted_price[0][0]}")
+class Evaluate():
+    def __init__(self):
+        inst = Dataset()
+        inst2 = Training_Dataset()
+        self.scaler = inst2.scaler
+        self.X_input = torch.tensor(inst.X_input, dtype=torch.float32)
+        self.model = LSTMModel()
+        self.weights = self.model.load_state_dict(torch.load("best_lstm_model.pth", weights_only=True))
+    
+    def eval(self):
+        self.model.eval()
+        with torch.no_grad():
+            predicted_price = self.model(self.X_input)
+        predicted_price = self.scaler.inverse_transform(predicted_price.detach().numpy())
+        return f"Predicted stock price for the next day: {predicted_price[0][0]}"
