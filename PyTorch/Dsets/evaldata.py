2a7dda31bfc9cb7964d053cbde1c31b67d7faf1f
Sami Souayah <souays@bu.edu>
2026-06-18 11:10:52 -0400

    Stuff

--- PyTorch/Dsets/evaldata.py
+++ PyTorch/Dsets/evaldata.py
@@ -2,6 +2,7 @@ import torch
 import yfinance as yf
 from transformations import Transformations
 import numpy as np
+import pandas as pd
 
 
 
@@ -18,9 +19,44 @@ class EvalData():
         self.volume_scaler = inst.volume_scaler
         self.ticker = ticker
         self.df = None
+        self.X_input = None
+        self.feature_df = None
         self.seql = inst.seql
 
     def find_ticker(self):
-        ticker = yf.Search(self.ticker)
+        ticker = yf.Ticker(self.ticker)
         df = ticker.history(period="5y")
-        self.df = df 
+        self.df = df
+    
+    def build_eval_features(self):
+        df = self.df.copy()
+        df["Volatility"] = df["High"] - df["Low"]
+        df = df[["Date", "Ticker", "Close", "Volume", "Volatility"]]
+        df["Date"] = pd.to_datetime(df["Date"])
+        df = df.sort_values(by=["Ticker", "Date"])
+        self.feature_df = df
+
+    def apply_scalers(self):
+        self.feature_df["Close_norm"] = self.price_scaler.transform(self.feature_df[["Close"]])
+        self.feature_df["Volatility_norm"] = self.volatility_scaler.transform(self.feature_df[["Volatility"]])
+        self.feature_df["Volume_norm"] = self.volume_scaler.transform(self.feature_df[["Volume"]])
+
+    def create_eval_sequence(self):
+        X = []
+        for i, j in self.feature_df.groupby("Ticker"):
+            j = j.sort_values("Date")
+
+            features = j[["Close_norm", "Volatility_norm", "Volume_norm"]].values
+            targets = j["Close_norm"].values
+
+            max_val = len(j) - self.seql + 1
+
+            for k in range(max_val):
+                x_window = features[k:]
+
+                if len(x_window) == self.seql:
+                    X.append(x_window)
+        self.X_input = np.array(X, dtype=np.float32)
+
+    def to_tensor(self):
+        self.X_input = torch.tensor(self.X_input, dtype=torch.float32)
